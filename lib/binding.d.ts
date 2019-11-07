b07a2a6c7919cd36e6b3de2937a16c44aabe4d64
shynome <17316043+shynome@users.noreply.github.com>
2019-11-07 17:44:07 +0800

    优化

--- lib/binding.d.ts
+++ lib/binding.d.ts
@@ -1,38 +1,54 @@
-import EventTarget from "./eventtarget";
 
-export class MediaStream extends EventTarget { }
-export class MediaStreamTrack extends EventTarget { }
-export class RTCAudioSink extends EventTarget { }
-export class RTCAudioSource { }
-export class RTCDataChannel extends EventTarget {
+declare class _MediaStream { }
+interface _MediaStream extends MediaStream { }
+export { _MediaStream as MediaStream }
+
+declare class _MediaStreamTrack { }
+interface _MediaStreamTrack extends MediaStreamTrack { }
+export { _MediaStreamTrack as MediaStreamTrack }
+
+export declare class RTCAudioSink { }
+export declare class RTCAudioSource { }
+
+declare class _RTCDataChannel { }
+interface _RTCDataChannel extends RTCDataChannel {
   send(data: any): void
 }
-export class RTCDtlsTransport extends EventTarget { }
-export class RTCIceTransport extends EventTarget { }
-export class RTCRtpReceiver { }
-export class RTCRtpSender { }
-export class RTCRtpTransceiver { }
-export class RTCSctpTransport extends EventTarget { }
-export class RTCVideoSink extends EventTarget { }
-export class RTCVideoSource { }
-export class getUserMedia { }
-export class i420ToRgba { }
-export class rgbaToI420 { }
-export class setDOMException { }
+export { _RTCDataChannel as RTCDataChannel }
 
-declare class _RTCPeerConnection extends RTCPeerConnection {
+declare class _RTCDtlsTransport { }
+interface _RTCDtlsTransport extends RTCDtlsTransport { }
+export { _RTCDtlsTransport as RTCDtlsTransport }
+
+declare class _RTCIceTransport { }
+interface _RTCIceTransport extends RTCIceTransport { }
+export { _RTCIceTransport as RTCIceTransport }
+
+declare class _RTCRtpReceiver { }
+interface _RTCRtpReceiver extends RTCRtpReceiver { }
+export { _RTCRtpReceiver as RTCRtpReceiver }
+
+declare class _RTCRtpSender { }
+interface _RTCRtpSender extends RTCRtpSender { }
+export { _RTCRtpSender as RTCRtpSender }
+
+declare class _RTCRtpTransceiver { }
+interface _RTCRtpTransceiver extends RTCRtpTransceiver { }
+export { _RTCRtpTransceiver as RTCRtpTransceiver }
+
+declare class _RTCSctpTransport { }
+interface _RTCSctpTransport extends RTCSctpTransport { }
+export { _RTCSctpTransport as RTCSctpTransport }
+
+export declare class RTCVideoSink { }
+export declare class RTCVideoSource { }
+export declare class getUserMedia { }
+export type i420ToRgba = any
+export type rgbaToI420 = any
+export type setDOMException = any
+
+declare class _RTCPeerConnection { }
+interface _RTCPeerConnection extends RTCPeerConnection {
   getReadyState(): any
 }
-export {
-  _RTCPeerConnection as RTCPeerConnection
-}
-
-// RTCAudioSource,
-// RTCRtpReceiver,
-// RTCRtpSender,
-// RTCRtpTransceiver,
-// RTCVideoSource,
-// getUserMedia,
-// i420ToRgba,
-// rgbaToI420,
-// setDOMException
+export { _RTCPeerConnection as RTCPeerConnection }
