381313f5850fd3036abeb9511d69b40249360b10
shynome <17316043+shynome@users.noreply.github.com>
2019-11-07 17:08:25 +0800

    EventTarget 是标准实现

--- lib/eventtarget.d.ts
+++ lib/eventtarget.d.ts
@@ -1,9 +1,5 @@
 
-declare class EventTarget {
-  _listeners: { [k: string]: Set<any> }
-  addEventListener(type: any, listener: any): void
-  dispatchEvent(event: any): void
-  removeEventListener(type: any, listener: any): void
+declare class EventTarget extends window.EventTarget {
 }
 
 export = EventTarget
