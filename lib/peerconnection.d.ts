ca3b113e8af4bfe77d7ec1f0ac365c56a4be4fc0
shynome <17316043+shynome@users.noreply.github.com>
2019-11-07 17:58:27 +0800

    优化

--- lib/peerconnection.d.ts
+++ lib/peerconnection.d.ts
@@ -1,52 +1,19 @@
 
-import EventTarget from "./eventtarget";
 import *as _webrtc from "./binding";
 import RTCSessionDescription from "./sessiondescription";
 
-declare class RTCPeerConnection<pc extends _webrtc.RTCPeerConnection = _webrtc.RTCPeerConnection> extends EventTarget {
+interface PortRange {
+  min: number
+  max: number
+}
 
-  // NOTE(mroberts): This is a bit of a hack.
-  ontrack: any
+interface RTCExtralConfiguration {
+  portRange: PortRange
+  sdpSemantics: string
+}
 
-  _pc: pc
-  canTrickleIceCandidates: pc['canTrickleIceCandidates']
-  connectionState: pc['connectionState']
-  currentLocalDescription: RTCSessionDescription
-  localDescription: RTCSessionDescription
-  pendingLocalDescription: RTCSessionDescription
-  currentRemoteDescription: RTCSessionDescription
-  remoteDescription: RTCSessionDescription
-  pendingRemoteDescription: RTCSessionDescription
-  signalingState: pc['signalingState']
-  // pc['getReadyState']
-  readyState: any
-  sctp: pc['sctp']
-  iceGatheringState: pc['iceGatheringState']
-  iceConnectionState: pc['iceConnectionState']
-  onconnectionstatechange: pc['onconnectionstatechange']
-  ondatachannel: pc['ondatachannel']
-  oniceconnectionstatechange: pc['oniceconnectionstatechange']
-  onicegatheringstatechange: pc['onicegatheringstatechange']
-  onnegotiationneeded: pc['onnegotiationneeded']
-  onsignalingstatechange: pc['onsignalingstatechange']
-
-  addIceCandidate: pc['addIceCandidate']
-  addTransceiver: pc['addTransceiver']
-  addTrack: pc['addTrack']
-  close: pc['close']
-  createDataChannel: pc['createDataChannel']
-  createOffer: pc['createOffer']
-  createAnswer: pc['createAnswer']
-  getConfiguration: pc['getConfiguration']
-  getReceivers: pc['getReceivers']
-  getSenders: pc['getSenders']
-  getTransceivers: pc['getTransceivers']
-  getStats: pc['getStats']
-  removeTrack: pc['removeTrack']
-  setConfiguration: pc['setConfiguration']
-  setLocalDescription: pc['setLocalDescription']
-  setRemoteDescription: pc['setRemoteDescription']
-
+declare class RTCPeerConnection extends _webrtc.RTCPeerConnection {
+  constructor(configuration?: RTCConfiguration & RTCExtralConfiguration)
 }
 
 export = RTCPeerConnection
